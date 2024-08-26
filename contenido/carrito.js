a8f167f783b4500e283adfdbfd54318d0eabe631
DLeonardoG <darkleonardog@gmail.com>
2024-08-26 09:02:54 -0500

    feat: :sparkles: Se mejoro el carrito
    el carrito se le anadio diseño
    
    By @DLeonardoG

--- contenido/carrito.js
+++ contenido/carrito.js
@@ -1,5 +1,37 @@
-let productosEnCarrito = localStorage.getItem("productos-en-carrito");
-productosEnCarrito = JSON.parse(productosEnCarrito);
+// let productosEnCarrito = localStorage.getItem("productos-en-carrito");
+// productosEnCarrito = JSON.parse(productosEnCarrito);
+
+const cargarPeliculas = async () => {
+    try {
+        const respuesta = await fetch("../../datos.json"); 
+        if (respuesta.status === 200) {
+            const datos = await respuesta.json();
+            console.log(datos)
+            return datos;   
+        } else {
+            console.log("Error al cargar los peliculas");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const mostrarPeliculaDescripcion = async () => {
+    const descripcion = document.getElementById("carrito-productos");
+    const peliculas = await cargarPeliculas();
+    let descripcionContenido = ``;
+    for (let i = 0; i < 3; i++) {
+        descripcionContenido += `
+        <figure>
+            <a class="miniaturas__peliculas" href="contenido/peliculas/descripcion/descripcion.html">
+            <img class="miniaturas__peliculas__imagen"  src="../../${peliculas[i].imagen}" alt="${peliculas[i].titulo}">
+            </a>
+            <figcaption><span class="miniaturas__peliculas__nombre">${peliculas[i].titulo}</span></figcaption>
+        </figure>`;
+    }
+    descripcion.innerHTML = descripcionContenido;
+    };
+// mostrarPeliculaDescripcion()
 
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoProductos = document.querySelector("#carrito-productos");
